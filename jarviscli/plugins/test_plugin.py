4a1fea2e631e0005aa4113973394aebb0d4f272a
Alex Doganis <72792044+AirlineDog@users.noreply.github.com>
2022-05-17 23:15:26 +0300

    Create test_plugin.py

---
+++ jarviscli/plugins/test_plugin.py
@@ -0,0 +1,16 @@
+from plugin import plugin
+from colorama import Fore
+
+
+@plugin("asdf")
+def asdf(jarvis, s):
+    """
+    prints asdf
+    """
+x = None
+i =2+1
+if i==5:
+  i = 6
+elif (x== None):
+  x = "asdf"
+print( x )
